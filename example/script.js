fc762a05cb3c9cb42e62bd96ff58a0a1a972b348
Kristian Kaa <kaakristian@gmail.com>
2016-12-28 11:13:56 +0100

    Updated example

--- example/script.js
+++ example/script.js
@@ -1,7 +1,16 @@
-import React from 'react';
 import Gun from 'gun/gun';
 
-alert("hej1");
-export const hej = () => {
-  alert("hej");
-};
+const gun = new Gun();
+
+// Watch the todos.
+gun.get('todos').map().val(todo => {
+  console.log('TODO', todo);
+});
+
+const todo1 = { name: "Clean house" };
+
+gun.get('todos').set(todo1);
+
+todo1.assignees = ['Kristian Kaa'];
+
+gun.get('todos').set(todo1);
